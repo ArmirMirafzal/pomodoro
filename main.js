c18d06d94c734b88e4c20049f253cd08f46bdb5d
sa1doff <murodilxonsaidov22@gmail.com>
2023-03-15 15:00:16 +0500

    12-27

--- main.js
+++ main.js
@@ -2,10 +2,26 @@
 const timer = document.querySelector(".main-time");
 const actionBtns = document.querySelectorAll(".main-menu-item");
 const startBtn = document.getElementById("startBtn");
+const shortBreak = document.querySelector('.shortBreak');
+const longBreak = document.querySelector('.longBreak')
+const pomodoro = document.querySelector('.pomodoro')
 
 let time = 1500;
 let intervalID;
 
+shortBreak.addEventListener('click', function(){
+	document.body.style.background = 'blue';
+	document.querySelector('.main').style.background = "darkblue"
+})
+
+longBreak.addEventListener('click', function(){
+	document.body.style.background = 'yellow';
+	document.querySelector('.main').style.background = "rgb(141, 180, 83)"
+})
+pomodoro.addEventListener('click', function(){
+	document.body.style.background = 'rgb(186, 73, 73)';
+	document.querySelector('.main').style.background = "#c15c5c"	
+})
 function timeConvertor(time) {
 	const minute = Math.floor(time / 60); // 1480 -> 24
 	const second = time % 60; // 1480 % 60 = 40
@@ -40,13 +56,7 @@ function handleStart() {
 function handleActionBtnClick(actionBtn) {
 	const currentTime = +actionBtn.getAttribute("time");
 	const isActive = actionBtn.classList.contains("active");
-	
-	// if(isActive){
-	// 	if(actionBtns[1].classList.contains(" shortBreak")){
-	// 		document.querySelector("body").style.background = "rgb(56, 133, 138)";
-	// 	}
-	// }
-
+    
 	if (!isActive) {
 		const isYes = startBtn.classList.contains("active") ? confirm(`${actionBtn.innerText} 🧐 ? `) : true;
 
