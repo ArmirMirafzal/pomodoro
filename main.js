7aa57fc3d59679981f07260e6e92ff52f69a665d
sa1doff <murodilxonsaidov22@gmail.com>
2023-03-21 11:50:25 +0500

    first commit

--- main.js
+++ main.js
@@ -24,8 +24,8 @@ pomodoro.addEventListener('click', function(){
 })
 
 function timeConvertor(time) {
-	const minute = Math.floor(time / 60); // 1480 -> 24
-	const second = time % 60; // 1480 % 60 = 40
+	const minute = Math.floor(time / 60);
+	const second = time % 60; 
 
 	let result = (minute < 10 ? "0" : "") + minute;
 	result += ":" + (second < 10 ? "0" : "") + second;
